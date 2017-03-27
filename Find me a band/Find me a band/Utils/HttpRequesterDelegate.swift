cf20ec18c89f0baf5d6126ed296e506511a4b192
Kalito98 <kaloyan.yanev98@gmail.com>
2017-03-27 22:07:31 +0300

    Added HttpRequesterDekegate

---
+++ Find me a band/Find me a band/Utils/HttpRequesterDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  HttpRequesterDelegate.swift
+//  Find me a band
+//
+//  Created by Kaloyan Yanev on 3/27/17.
+//  Copyright © 2017 Kaloyan Yanev. All rights reserved.
+//
+
+import Foundation
+
+protocol HttpRequesterDelegate {
+    func didReciveData(data: Any)
+    func didDeleteData()
+}
+
+extension HttpRequesterDelegate {
+    func didReciveData(data: Any) {
+        
+    }
+    
+    func didDeleteData() {
+        
+    }
+}
