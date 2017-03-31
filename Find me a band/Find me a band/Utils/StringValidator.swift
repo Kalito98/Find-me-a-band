d1a6a6aa87bc66c3c807f256639b7cc48dde8307
Kalito98 <kaloyan.yanev98@gmail.com>
2017-03-31 15:46:43 +0300

    Started validator and added pod Toaster

---
+++ Find me a band/Find me a band/Utils/StringValidator.swift
@@ -0,0 +1,14 @@
+//
+//  StringValidator.swift
+//  Find me a band
+//
+//  Created by Kaloyan Yanev on 3/31/17.
+//  Copyright © 2017 Kaloyan Yanev. All rights reserved.
+//
+
+import Foundation
+
+class StringValidator {
+    func isValid(withString string: String, withMaxLenght maxLenght: int, andMinLenght minLenght: int ) -> Bool {
+    }
+}
